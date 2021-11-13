1321d3af76a012e7808f7074161c1996215d42a5
박혜원 <79081836+phw-plain@users.noreply.github.com>
2021-11-13 23:21:01 +0900

    Stage Update 3

--- js/ai.js
+++ js/ai.js
@@ -1,7 +1,7 @@
-var x1 = 10;
-var x2 = 1000;
-var step1 = 10;
-var step2 = 10;
+var x1 = 1600;
+var x2 = 10;
+var step1 = 20;
+var step2 = 20;
 var st;
 var autoBt;
 var ai1;
@@ -11,16 +11,18 @@ function init(){
     autoBt = document.getElementById("autoBt");
     ai1 = document.getElementById("ai1");
     ai2 = document.getElementById("ai2");
+    ai3 = document.getElementById("ai3");
     moveAuto()
 }
 
 function manual(){
-    x1 += step1;
-    step1=(x1>=1000 || x1 <= 10)? -step1:step1;
-    step2=(x2>=1000 || x2 <= 10)? -step2:step2;
     x2 += step2;
+    step1=(x1>=1600 || x1 <= 10)? -step1:step1;
+    step2=(x2>=1600 || x2 <= 10)? -step2:step2;
+    x1 += step1;
     ai1.style.left = x1+"px";
     ai2.style.left = x2+"px";
+    ai3.style.left = x1+"px";
 }
 
 function moveAuto(){
