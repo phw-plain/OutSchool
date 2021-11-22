62e18198ef452e0c574236f5586d12864896209e
박혜원 <79081836+phw-plain@users.noreply.github.com>
2021-11-22 23:28:27 +0900

    Stage Update 6

--- js/right.js
+++ js/right.js
@@ -1,11 +1,12 @@
 let autoBlind;
-
+let time = 8000;
 function init(){
     autoBlind = document.body.querySelector('#blind');
     blindAuto()
 }
 
 function manual(){
+    time = 8000;
     document.body.classList.add('.blind')
     autoBlind.style.opacity = '1'
     // document.body.querySelector('.character').style.display = 'none';
@@ -14,9 +15,9 @@ function manual(){
         // document.body.querySelector('.character').style.display = 'block';
         document.body.classList.remove('.blind')
         autoBlind.style.opacity = '0'
-    }, 5000);
+    }, 3000);
 }
 
 function blindAuto(){
-    let st = setInterval("manual()", 10000);
+    let st = setInterval("manual()", time);
 }
