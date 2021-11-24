8d812d36c36d94702984ae9f637321c188cc7ac9
박혜원 <79081836+phw-plain@users.noreply.github.com>
2021-11-24 22:57:04 +0900

    Stage Update 8

--- js/right.js
+++ js/right.js
@@ -8,11 +8,17 @@ function init(){
 function manual(){
     time = 6000;
     document.body.classList.add('.blind')
+    document.body.querySelector('.box').style.opacity = '0';
+    document.body.querySelector('.obj').style.opacity = '0';
+    document.body.querySelector('.obj2').style.opacity = '0';
     autoBlind.style.opacity = '1'
     // document.body.querySelector('.character').style.display = 'none';
 
     setTimeout(function(){
         // document.body.querySelector('.character').style.display = 'block';
+        document.body.querySelector('.box').style.opacity = '1';
+        document.body.querySelector('.obj').style.opacity = '1';
+        document.body.querySelector('.obj2').style.opacity = '1';
         document.body.classList.remove('.blind')
         autoBlind.style.opacity = '0'
     }, 2000);
