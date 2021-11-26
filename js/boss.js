6f33a87c3dbd83d42d0da4cfc0f687aca15a300c
박혜원 <79081836+phw-plain@users.noreply.github.com>
2021-11-27 04:16:07 +0900

    GameStart Update 3

--- js/boss.js
+++ js/boss.js
@@ -3,20 +3,17 @@ let open1 = 0
 let check = 0, check1 = 0
 let clear = false
 
+const click = new Audio(); 
+click.src = "../mp3/button2.mp3"; 
+
 function btnclick(idx){
-	if(check != 0){
-		let btn = document.body.querySelector(`#btn${check}`)
-		btn.style.color = "white"
-	}
+	click.play();
 	check = idx
-	let btn = document.body.querySelector(`#btn${idx}`)
-	btn.style.color = "black"
+	okay()
 }
 
 function okay() {
-	if(check == 0){
-		alert("방 번호를 선택하여 주세요!")
-	} else if(check != 0 && check1 == 0) {
+	if(check != 0 && check1 == 0) {
 		if(confirm(`${check}번째 방을 선택하시겠습니까?`)){
 			for(let i = 1; i<=3; i++){
 				if(i != answer && i != check)
@@ -24,9 +21,8 @@ function okay() {
 			}
 			check1 = check
 			document.body.querySelector(`#door${open1}`).src = `../icon/door${open1}_open.png`
-			
-		} else {
-			alert('취소 되었습니다.')
+			let btn = document.body.querySelector(`#btn${check}`)
+			btn.style.color = "black"
 		}
 	} else if(open1 == check) {
 		alert(`공개된 방 ${open1}은(는) 더 이상 선택할 수 없습니다.`)
@@ -34,21 +30,20 @@ function okay() {
 		if(confirm(`${check}번째 방으로 선택을 유지하시겠습니까?`)){
 			if(check == answer) clear = true
 			clearCheck()
-		} else{
-			alert('취소 되었습니다.')
 		}
 	} else if(check != check1) {
 		if(confirm(`${check}번째 방으로 선택을 변경하시겠습니까?`)){
 			if(check == answer) clear = true
 			clearCheck()
-		} else{
-			alert('취소 되었습니다.')
 		}
 	}
 }
 
 function clearCheck(){
 	if(clear){
+		let btn = document.body.querySelector(`#btn${check1}`)
+		btn.style.color = "white"
+
 		alert(`정답은 ${answer}!! 당신은 도전에 성공하였습니다.`)
 		for(let i=1; i<=3; i++){
 			if(i != open1)
--- js/stage.js
+++ js/stage.js
@@ -1,6 +1,10 @@
+const door = new Audio()
+door.src = "../mp3/door.mp3"
+door.play()
 function die(){
-    location.href="../GameOver.html";
+    location.href="../GameOver.html"
 }
+
 function nextstage(stage){
-		location.href=`stage${stage}.html`;
+	location.href=`stage${stage}.html`
 }
